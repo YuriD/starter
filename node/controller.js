bfa7d8e1492881b04a2dbc44b6e57539a86b55dd
YuriD <yurida@mail.ru>
2013-06-25 14:21:18 +0700

    Update controller.js

--- node/controller.js
+++ node/controller.js
@@ -1,40 +1,62 @@
 var execFile = require('child_process').execFile;
+var fs = require('fs');
+var async = require('async');
 
 /******************************************************************************/
 //  CONFIGURATION 
 //   The root of sample data
-var collectDataRoot = __dirname + "/sampledata";
-//TODO: add other config here 
-
+var collectDataRoot = __dirname + "/sampledata", fn = [];
+if(!fs.existsSync(collectDataRoot)) fs.mkdirSync(collectDataRoot);
+for (var i = 1; i < 289; i++) {
+  fn.push(collectDataRoot + '/host_' + (i.toString().length<3?(i.toString().length<2?'00'+i:'0'+i):i));
+  fs.writeFileSync(fn[i-1], +Math.random().toFixed(3));
+}
 /******************************************************************************/
 
-var getStuff = exports.getStuff = function(req, res, next) {
-   var cmd = "ls";
-   var args = ["-l"];
-   var child = execFile(cmd, args, function(err, stdout, stderr) {
-          if (err) {
+var getData = exports.getData = function (req, res, next) {
+  async.map(fn, fs.readFile, function (err, results) {
+    if(err) {
+      console.log("Got error: " + err);
+      next(err);
+    }
+//  console.log(JSON.stringify(formatOutput(results)));
+    res.json(formatOutput(results));
+  });
+}
+
+/*
+var getStuff = exports.getStuff = function (req, res, next) {
+    var cmd = "ls";
+    var args = ["-l"];
+    var child = execFile(cmd, args, function (err, stdout, stderr) {
+        if (err) {
             console.log("Error:  ", err);
             console.log("stderr: ", stderr);
             next(err);
-         }
-         res.json(formatOutput(stdout)); 
-      });
+        }
+        res.json(formatOutput(stdout));
+    });
 }
-
+*/
 
 var formatOutput = function (data) {
-   //TODO: process any data here....
-   return data;
+  for (var i = 0; i < data.length; i++) {
+    data[i] = +data[i].toString();
+  }
+  return data;
 };
+
+//getData();
 
 // Quick & dirty testing
 // Comment in and out
+/*
 var resmock = { json: function (data) {
    console.log(JSON.stringify(data, null, 2));
 }};
 var nextmock = function(err) { console.log(err); }
 var reqmock = { params: { id:"123"}, query:{p1: 1, p2: "param-1"} };
 getStuff(reqmock,resmock, nextmock);
-
+*/
 
 
